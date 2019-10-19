27457109a77bd8f8c71ef96d02f4c10af5f705f1
hacker-pixel <56743722+hacker-pixel@users.noreply.github.com>
2019-10-19 03:00:30 +0300

    firsthack

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready( function(){
+
+      $('.button').on('click',function(){
+      	$('.modelWindow').toggle('active');
+      
+      });
+      $('.close').on('click', function(){
+            $('.modelWindow').toggle('active');
+      });
+
+
+});
